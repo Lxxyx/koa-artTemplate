9c275662bf108078eab928faa90ae897fa635f8e
Lxxyx <841380530@qq.com>
2016-08-21 15:11:30 +0800

    fix(fix cover bug):

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ function render (filename, data) {
 }
 
 module.exports = function (dir, ext) {
-  if (!fs.statSync(dir).isDirectory()) {
-    return console.error('koa art-template must put in directory with absolute path')
-  }
   template.config('base', dir)
   template.config('extname', ext || '.html')
   return function *(next) {
