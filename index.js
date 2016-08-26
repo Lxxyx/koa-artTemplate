0b0cb82e4107a46149d6180a44b1448137f8807c
刘子健 <841380530@qq.com>
2016-08-26 13:22:24 +0800

    fix(remove useless code):

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const template = require('art-template')
-const fs = require('fs')
-const path = require('path')
 
 function render (filename, data) {
   return template(filename, data)
@@ -9,7 +7,7 @@ function render (filename, data) {
 module.exports = function (dir, ext) {
   template.config('base', dir)
   template.config('extname', ext || '.html')
-  return function *(next) {
+  return function * (next) {
     this.render = render
     yield next
   }
